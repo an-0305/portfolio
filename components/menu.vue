97a8f2015f4aa0413752ca72a3ddc02083a51d10
Akari Nakamura <AN@an.local>
2020-02-23 10:07:03 +0900

    add: ファイルを追加

---
+++ components/menu.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="c-menu">
+    <ul class="menu-list">
+      <li class="menu-item">
+        <a href="">about</a>
+      </li>
+      <li class="menu-item">
+        <a href="">skills</a>
+      </li>
+      <li class="menu-item">
+        <a href="">work</a>
+      </li>
+    </ul>
+  </div>
+</template>
