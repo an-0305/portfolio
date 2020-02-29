f85afe6584e6e1ccd1729dde8470d2014a378319
Akari Nakamura <AN@an.local>
2020-02-29 18:01:00 +0900

    add: ハンバーガーの3本線を追加

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,10 @@
 <template>
   <header class="c-header">
     <h1 class="title"><a href="/">akari.life</a></h1>
+    <div class="navi">
+      <label for="humberger">
+        <span></span>
+      </label>
+    </div>
   </header>
 </template>
