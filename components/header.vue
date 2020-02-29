269f5d6f63cbd32348c887dd248b031c8b7e2a7a
Akari Nakamura <AN@an.local>
2020-03-01 08:36:45 +0900

    add: animationを追加

--- components/header.vue
+++ components/header.vue
@@ -1,10 +1,13 @@
 <template>
   <header class="c-header">
-    <h1 class="title"><a href="/">akari.life</a></h1>
+    <h1 class="title">
+      <a href="/">akari.life</a>
+    </h1>
     <div class="navi">
-      <label for="humberger">
-        <span></span>
-      </label>
+      <span />
     </div>
   </header>
 </template>
+
+<script>
+</script>
