1079540d074322a9464b067ee4827b5a31e81287
Akari Nakamura <AN@an.local>
2020-03-01 11:21:00 +0900

    add: aboutページを追加

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="o-container p-about">
+    <h2 class="title">about</h2>
+    <div class="content">
+      <div class="img">
+        <img src="../assets/images/about.png" alt="">
+      </div>
+      <p>1996年 福岡県出身</p>
+      <p>大学は理学部生物学科出身で細胞生物学の研究を行う</p>
+      <p>元々モノを作ることが好きなことや、場所や時間を選ばずに自分のスキルを伸ばしながら働けることに魅力を感じ、未経験でエンジニアとして入社</p>
+      <p>入社1年目はフロントエンドエンジニアとして働き、2年目からは広告運用・ECコンサルティング業務に携わる</p>
+      <p>趣味は旅行とアクアリウム</p>
+    </div>
+  </div>
+</template>
