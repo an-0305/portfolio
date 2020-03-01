ade099eb51b1414e0887dcf9bd89518f4d227fa9
Akari Nakamura <AN@an.local>
2020-03-01 09:40:20 +0900

    add: ハンバーガーメニューアイコンの切り替え機能を追加

--- components/header.vue
+++ components/header.vue
@@ -3,14 +3,20 @@
     <h1 class="title">
       <a href="/">akari.life</a>
     </h1>
-    <div @click="onClickNavi" class="navi">
+    <div @click="onClickNavi" v-bind:class="{active: isMenuActive}" class="navi">
       <span />
     </div>
   </header>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
+  computed: {
+    ...mapState({
+      isMenuActive: state => state.isMenuActive
+    })
+  },
   methods: {
     onClickNavi () {
       this.$store.commit('toggleMenuActive')
