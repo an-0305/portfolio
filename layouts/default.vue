5472dbad7dbc8dbcca39f60ce59408e06a35e43f
Akari Nakamura <AN@an.local>
2020-03-07 08:52:00 +0900

    add: footerファイルの追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,17 +3,20 @@
     <Header />
     <Menu />
     <nuxt />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '~/components/header.vue'
 import Menu from '~/components/menu.vue'
+import Footer from '~/components/footer.vue'
 
 export default {
   components: {
     Header,
-    Menu
+    Menu,
+    Footer
   }
 }
 
