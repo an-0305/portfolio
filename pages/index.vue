4240bcd16c0fad7dba05b66ad11c923f3ae5e113
Akari Nakamura <AN@an.local>
2020-03-07 17:53:01 +0900

    add: スキルセクションのdesktop用のレイアウトを追加

--- pages/index.vue
+++ pages/index.vue
@@ -19,20 +19,22 @@
     <section class="content skill">
       <h2 class="title">skill</h2>
       <div class="skill-wrap">
-        <h3 class="head">フロントエンド</h3>
-        <div class="icon"><img src="../assets/images/develop.svg" alt=""></div>
-        <p>デザインを元にHTML/CSS/Javascriptの設計・実装。特に共通機能はコンポーネント化するなど、保守性に気をつけた設計・実装を行っています。</p>
-      </div>
-      <div class="skill-wrap">
-        <h3 class="head">マーケティング</h3>
-        <div class="icon"><img src="../assets/images/marketing.svg" alt=""></div>
-        <p>Google Analyticsを用いてサイト内分析を行うことができます。
-  Google Analyticsだけでは追えないデータはスクレイピングによりデータ収集したり、受注データなどの各種データベースからSQLにより抽出・集計しています。</p>
-      </div>
-      <div class="skill-wrap">
-        <h3 class="head">ツール</h3>
-        <div class="icon"><img src="../assets/images/tool.svg" alt=""></div>
-        <p>開発はバージョン管理ツールとしてGitを利用、通常業務にはSpreadSheet(GAS含む)などG Suite製品をよく利用しています。マーケティングにおいてはGoogle Data Portalでレポーティングを行なっています。</p>
+        <div class="skill-item">
+          <h3 class="head">フロントエンド</h3>
+          <div class="icon"><img src="../assets/images/develop.svg" alt=""></div>
+          <p>デザインを元にHTML/CSS/Javascriptの設計・実装。特に共通機能はコンポーネント化するなど、保守性に気をつけた設計・実装を行っています。</p>
+        </div>
+        <div class="skill-item">
+          <h3 class="head">マーケティング</h3>
+          <div class="icon"><img src="../assets/images/marketing.svg" alt=""></div>
+          <p>Google Analyticsを用いてサイト内分析を行うことができます。
+    Google Analyticsだけでは追えないデータはスクレイピングによりデータ収集したり、受注データなどの各種データベースからSQLにより抽出・集計しています。</p>
+        </div>
+        <div class="skill-item">
+          <h3 class="head">ツール</h3>
+          <div class="icon"><img src="../assets/images/tool.svg" alt=""></div>
+          <p>開発はバージョン管理ツールとしてGitを利用、通常業務にはSpreadSheet(GAS含む)などG Suite製品をよく利用しています。マーケティングにおいてはGoogle Data Portalでレポーティングを行なっています。</p>
+        </div>
       </div>
       <div class="button-wrap">
         <nuxt-link to="skill" class="c-button">View More</nuxt-link>
