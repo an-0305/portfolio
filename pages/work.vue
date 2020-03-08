14e95ebd39329bdddc901e2c698f6f29ec5f4d0c
Akari Nakamura <AN@an.local>
2020-03-08 18:17:00 +0900

    add: スタイルの調整

--- pages/work.vue
+++ pages/work.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="p-work o-container">
-    <h2 class="title">work</h2>
+    <h2 class="title">
+      work
+    </h2>
     <div class="content">
-      <section>
-        <h3>1.アパレル系ECサイトの構築・保守</h3>
+      <section class="content-section">
+        <h3 class="head">
+          1.アパレル系ECサイトの構築・保守
+        </h3>
         <p>HTML/CSSによるテンプレートコーディング</p>
         <p>CSSフレームワークはITCSSをベースとし、最小限の工数で多数のECサイトをオープンさせるため共通機能のコンポーネント化を行った実装。</p>
         <p>ajaxにより外部の会員データシステム連携の実装を行う。JavascriptはES2015で記述。</p>
       </section>
-      <section>
-        <h3>2.アパレル系ECサイトのコンサルティング</h3>
+      <section class="content-section">
+        <h3 class="head">
+          2.アパレル系ECサイトのコンサルティング
+        </h3>
         <p>売上向上のため、Google Analyticsでサイト内解析を行う</p>
         <p>過去の受注データからの購入者分析</p>
         <p>サイトにGoogle Tag Managerを設置し、イベント計測、サイト構造の改善提案</p>
