b6268840e010f0b95bcaddf59d915c478e06a0d3
Akari Nakamura <AN@an.local>
2020-03-08 15:35:30 +0900

    add: スタイル調整

--- pages/skill.vue
+++ pages/skill.vue
@@ -4,34 +4,95 @@
     <div class="content">
       <section class="content-section">
         <h3 class="head">フロントエンド</h3>
-        <ul class="skill-items">
-          <li class="item">HTML/CSS
-            <small class="notice">アパレルECサイトのコーディング</small>
-          </li>
-          <li class="item">Javascript
-            <small class="notice">アパレルECサイトのコーディング</small>
-          </li>
-          <li class="item">Nuxt.js
-            <small class="notice">当サイトのコーディング</small>
-          </li>
-        </ul>
+          <div class="skill-wrap">
+            <div class="skill-wrap-table">
+              <table class="skill-table">
+                <tr>
+                  <th>HTML</th>
+                  <td>★★★</td>
+                </tr>
+                <tr>
+                  <th>CSS(SCSS)</th>
+                  <td>★★★</td>
+                </tr>
+                <tr>
+                  <th>Javascript</th>
+                  <td>★★</td>
+                </tr>
+                <tr>
+                  <th>Vue.js</th>
+                  <td>★</td>
+                </tr>
+                <tr>
+                  <th>Nuxt.js</th>
+                  <td>★</td>
+                </tr>
+              </table>
+            </div>
+            <div class="skill-wrap-description">
+              <p class="skill-description">デザインを元にHTML/CSS/Javascriptの設計・実装。特に共通機能はコンポーネント化するなど、保守性に気をつけた設計・実装を行っています。</p>
+            </div>
+          </div>
       </section>
       <section class="content-section">
         <h3 class="head">マーケティング</h3>
-        <ul class="skill-items">
-          <li class="item">Google Analytics
-            <small class="notice">アパレルECのサイト内解析</small>
-          </li>
-          <li class="item">Google Tag Mangaer
-            <small class="notice">広告タグの管理・イベント計測</small>
-          </li>
-          <li class="item">SQL
-            <small class="notice">テーブル結合・集計関数・サブクエリ</small>
-          </li>
-          <li class="item">Python
-            <small class="notice">スクレイピング</small>
-          </li>
-        </ul>
+        <div class="skill-wrap">
+          <div class="skill-wrap-table">
+            <table class="skill-table">
+              <tr>
+                <th>Google Analytics</th>
+                <td>★★</td>
+              </tr>
+              <tr>
+                <th>Google Tag Manager</th>
+                <td>★★</td>
+              </tr>
+              <tr>
+                <th>SQL</th>
+                <td>★★</td>
+              </tr>
+              <tr>
+                <th>Python</th>
+                <td>★</td>
+              </tr>
+            </table>
+          </div>
+          <div class="skill-wrap-description">
+            <p class="skill-description">Google Analyticsを用いてサイト内分析を行うことができます。 Google Analyticsだけでは追えないデータはスクレイピングによりデータ収集したり、受注データなどの各種データベースからSQLにより抽出・集計しています。</p>
+          </div>
+        </div>
+      </section>
+      <section class="content-section">
+        <h3 class="head">ツール</h3>
+        <div class="skill-wrap">
+          <div class="skill-wrap-table">
+            <table class="skill-table">
+              <tr>
+                <th>Google Spreadsheet(GAS)</th>
+                <td>★★</td>
+              </tr>
+              <tr>
+                <th>Googleデータポータル</th>
+                <td>★★</td>
+              </tr>
+              <tr>
+                <th>Backlog</th>
+                <td>★★</td>
+              </tr>
+              <tr>
+                <th>Git</th>
+                <td>★★</td>
+              </tr>
+              <tr>
+                <th>Sketch</th>
+                <td>★</td>
+              </tr>
+            </table>
+          </div>
+          <div class="skill-wrap-description">
+            <p class="skill-description">開発はバージョン管理ツールとしてGitを利用、通常業務にはSpreadSheet(GAS含む)などG Suite製品をよく利用しています。マーケティングにおいてはGoogle Data Portalでレポーティングを行なっています。</p>
+          </div>
+        </div>
       </section>
     </div>
   </div>
